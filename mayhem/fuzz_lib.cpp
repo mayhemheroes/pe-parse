2d56ced6eb28f48804f7280131a194c6929cdc87
bcapuano <bcapuano@asu.edu>
2023-04-21 21:16:22 -0400

    Fixed instrumentation

--- mayhem/fuzz_lib.cpp
+++ mayhem/fuzz_lib.cpp
@@ -4,10 +4,6 @@
 
 extern "C" int LLVMFuzzerTestOneInput(uint8_t *data, size_t size) {
   peparse::parsed_pe* p = peparse::ParsePEFromPointer(data, size);
-  if (p == nullptr) {
-    return -1;
-  }
-  p->peHeader.dos.e_cblp;
   peparse::DestructParsedPE(p);
   return 0;
 }
